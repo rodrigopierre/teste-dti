0d756f83bec576be02ba9d5e1eb7bec84a1ee335
Rodrigo Pierre <rodrigopierre05@gmail.com>
2024-09-11 20:25:49 -0300

    feat: botão de delete (html)

--- client/src/Components/Day.tsx
+++ client/src/Components/Day.tsx
@@ -1,3 +1,4 @@
+import Lembrete from './Lembrete';
 import './Style/Day.css'
 
 
@@ -9,7 +10,11 @@ interface DayProps {
 const Day: React.FC<DayProps> = ({ date }) => {
 
   return (
-    <h2>{date}</h2>
+    <>
+        <h2>{date}</h2>
+        <Lembrete />
+    </>
+
   )
 }
 
---
+++ client/src/Components/Lembrete.tsx
@@ -0,0 +1,12 @@
+
+
+const Lembrete = () => {
+  return (
+    <div>
+        <p>Exemplo de lembrete</p>
+        <button className="delete">x</button>
+    </div>
+  )
+}
+
+export default Lembrete
