ffbff0afee9d6e441cbb74911c378ccd51397b96
Rodrigo Pierre <rodrigopierre05@gmail.com>
2024-09-13 08:53:05 -0300

    feat: front-end delete lembrete

--- client/src/Components/Reminder.tsx
+++ client/src/Components/Reminder.tsx
@@ -1,3 +1,4 @@
+import { api } from '../Services/api';
 import './Style/Reminder.css'
 
 
@@ -8,10 +9,18 @@ interface ReminderProps {
 
 
 const Reminder: React.FC<ReminderProps> = ({ title, id }) => {
+
+  function deleteClick() {
+    api.delete(`/reminders/${id}`)
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+  }
+
+
   return (
     <div className='reminder-container'>
         <p>{title}</p>
-        <button className="delete-btn">x</button>
+        <button className="delete-btn" onClick={() => deleteClick()}>x</button>
     </div>
   )
 }
